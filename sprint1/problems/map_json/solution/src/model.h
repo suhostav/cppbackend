4bf2e4399317bf866d6bcce28bbf62f415b7e04b
unknown <vsuhostav@gmail.com>
2024-06-21 16:58:36 +0300

    map_json

---
+++ sprint1/problems/map_json/solution/src/model.h
@@ -0,0 +1,276 @@
+#pragma once
+#include <format>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+#include "tagged.h"
+
+namespace model {
+
+using Dimension = int;
+using Coord = Dimension;
+
+struct Point {
+    Coord x, y;
+};
+
+struct Size {
+    Dimension width, height;
+};
+
+struct Rectangle {
+    Rectangle(Point p, Size s): position(p), size(s){}
+    Point position;
+    Size size;
+};
+
+struct Offset {
+    Dimension dx, dy;
+};
+
+class Road {
+    struct HorizontalTag {
+        explicit HorizontalTag() = default;
+    };
+
+    struct VerticalTag {
+        explicit VerticalTag() = default;
+    };
+
+public:
+    constexpr static HorizontalTag HORIZONTAL{};
+    constexpr static VerticalTag VERTICAL{};
+
+    Road(HorizontalTag, Point start, Coord end_x) noexcept
+        : start_{start}
+        , end_{end_x, start.y} {
+    }
+
+    Road(VerticalTag, Point start, Coord end_y) noexcept
+        : start_{start}
+        , end_{start.x, end_y} {
+    }
+
+    bool IsHorizontal() const noexcept {
+        return start_.y == end_.y;
+    }
+
+    bool IsVertical() const noexcept {
+        return start_.x == end_.x;
+    }
+
+    Point GetStart() const noexcept {
+        return start_;
+    }
+
+    Point GetEnd() const noexcept {
+        return end_;
+    }
+
+    std::string ToJson() const{
+        std::stringstream ss;
+        ss << "{ ";
+        if(IsHorizontal()){
+            ss << "\"x0\": "<< start_.x
+               << ", \"y0\": " << start_.y
+               << ", \"x1\": " << end_.x;
+        } else {
+            ss << "\"x0\": "<< start_.x
+               << ", \"y0\": " << start_.y
+               << ", \"y1\": " << end_.y;
+        }
+        ss << " }";
+        return ss.str();
+    }
+
+private:
+    bool IsHorizontal(){
+        return start_.y == end_.y;
+    }
+    Point start_;
+    Point end_;
+};
+
+class Building {
+public:
+    explicit Building(Rectangle bounds) noexcept
+        : bounds_{bounds} {
+    }
+
+    const Rectangle& GetBounds() const noexcept {
+        return bounds_;
+    }
+
+    std::string ToJson() const{
+        std::stringstream ss;
+        ss << "{ ";
+        ss << "\"x\": " << bounds_.position.x
+           << ", \"y\": " << bounds_.position.y
+           << ", \"w\": " << bounds_.size.width
+           << ", \"h\": " << bounds_.size.height
+           << " }";
+        return ss.str();      
+    }
+
+private:
+    Rectangle bounds_;
+};
+
+class Office {
+public:
+    using Id = util::Tagged<std::string, Office>;
+
+    Office(Id id, Point position, Offset offset) noexcept
+        : id_{std::move(id)}
+        , position_{position}
+        , offset_{offset} {
+    }
+
+    const Id& GetId() const noexcept {
+        return id_;
+    }
+
+    Point GetPosition() const noexcept {
+        return position_;
+    }
+
+    Offset GetOffset() const noexcept {
+        return offset_;
+    }
+
+    std::string ToJson(){
+        std::stringstream ss;
+        ss << "{ ";
+        ss << "\"id\": \"" << *id_ 
+           << ", \"x\": " << position_.x
+           << ", \"y\": " << position_.y
+           << ", \"offsetX\": " << offset_.dx
+           << ", \"offsetY\": " << offset_.dy
+           << " }";
+        return ss.str();
+    }
+
+private:
+    Id id_;
+    Point position_;
+    Offset offset_;
+};
+
+class Map {
+public:
+    using Id = util::Tagged<std::string, Map>;
+    using Roads = std::vector<Road>;
+    using Buildings = std::vector<Building>;
+    using Offices = std::vector<Office>;
+
+    Map(Id id, std::string name) noexcept
+        : id_(std::move(id))
+        , name_(std::move(name)) {
+    }
+
+    const Id& GetId() const noexcept {
+        return id_;
+    }
+
+    const std::string& GetName() const noexcept {
+        return name_;
+    }
+
+    const Buildings& GetBuildings() const noexcept {
+        return buildings_;
+    }
+
+    const Roads& GetRoads() const noexcept {
+        return roads_;
+    }
+
+    const Offices& GetOffices() const noexcept {
+        return offices_;
+    }
+
+    void AddRoad(const Road& road) {
+        roads_.emplace_back(road);
+    }
+
+    void AddBuilding(const Building& building) {
+        buildings_.emplace_back(building);
+    }
+
+    void AddOffice(Office office);
+
+    std::string ToJson() const{
+        using namespace std::literals;
+        std::stringstream ss;
+        ss << "{\n";
+        ss << "\t\"id\": " << *id_ << ",\n";
+        ss << "\t\"name\": " << name_ << ",\n";
+        ss << "\t\"roads\": ["s;
+        auto roads = GetRoads();
+        bool first = true;
+        for(auto road : roads){
+            if(first){ first = false; } else { ss << ','; }
+            ss << "\n\t\t";
+            ss << road.ToJson();
+        }
+        ss << "\n\t],\n";
+        auto buildings = GetBuildings();
+        ss << "\t\"buildings\": ["s;
+        first = true;
+        for(auto building : buildings){
+            if(first){ first = false; } else { ss << ','; }
+            ss << "\n\t\t";
+            ss << building.ToJson();
+        }
+        ss << "\n\t],";
+        auto offices = GetOffices();
+        ss << "\n\t\"offices\": ["s;
+        first = true;
+        for(auto office : offices){
+            if(first){ first = false; } else { ss << ','; }
+            ss << "\n\t\t";
+            ss << office.ToJson();
+        }
+        ss << "\n\t]";
+        ss << "\n}";
+        return ss.str();
+    }
+
+private:
+    using OfficeIdToIndex = std::unordered_map<Office::Id, size_t, util::TaggedHasher<Office::Id>>;
+
+    Id id_;
+    std::string name_;
+    Roads roads_;
+    Buildings buildings_;
+
+    OfficeIdToIndex warehouse_id_to_index_;
+    Offices offices_;
+};
+
+class Game {
+public:
+    using Maps = std::vector<Map>;
+
+    void AddMap(Map map);
+
+    const Maps& GetMaps() const noexcept {
+        return maps_;
+    }
+
+    const Map* FindMap(const Map::Id& id) const noexcept {
+        if (auto it = map_id_to_index_.find(id); it != map_id_to_index_.end()) {
+            return &maps_.at(it->second);
+        }
+        return nullptr;
+    }
+private:
+    using MapIdHasher = util::TaggedHasher<Map::Id>;
+    using MapIdToIndex = std::unordered_map<Map::Id, size_t, MapIdHasher>;
+
+    std::vector<Map> maps_;
+    MapIdToIndex map_id_to_index_;
+};
+
+}  // namespace model
---
+++ sprint1/problems/map_json/solution/src/request_handler.h
@@ -0,0 +1,124 @@
+#pragma once
+#include <format>
+#include "http_server.h"
+#include "model.h"
+
+namespace http_handler {
+namespace beast = boost::beast;
+namespace http = beast::http;
+using namespace std::literals;
+
+using StringResponse = http::response<http::string_body>;
+
+class RequestHandler {
+public:
+    explicit RequestHandler(model::Game& game)
+        : game_{game} {
+    }
+
+    RequestHandler(const RequestHandler&) = delete;
+    RequestHandler& operator=(const RequestHandler&) = delete;
+
+    template <typename Body, typename Allocator, typename Send>
+    void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+        // Обработать запрос request и отправить ответ, используя send
+        const std::string_view method_str{req.method_string()};
+        const auto text_response = [&req, this](http::status status, std::string_view text, std::string_view content_type) {
+            return this->MakeStringResponse(status, text, req.version(), req.keep_alive(), content_type);
+        };
+        StringResponse response{};
+        if( method_str == HEAD_STR || method_str == GET_STR){
+            std::string body;
+            if( method_str == GET_STR){
+                const std::string maps_list_request{"/api/v1/maps"};
+                const std::string map_request{"/api/v1/maps/"};
+                const std::string bad_api_request{"/api/"};
+                std::string target{req.target()};
+                std::cout << target << std::endl;
+                if(target == maps_list_request){
+                    body = MapsListResponse();
+                    response =  text_response(http::status::ok, body, ContentType::APPLICATION_JSON);
+                } else if(target.starts_with(map_request) && target.size() > map_request.size()){
+                    std::string map_id{target.substr(map_request.size())};
+                    body = MapResponse(map_id);
+                    response =  text_response(http::status::ok, body, ContentType::APPLICATION_JSON);
+                } else if(target.starts_with(bad_api_request)){
+                    body = BadRequest();
+                    response = text_response(http::status::bad_request, body, ContentType::APPLICATION_JSON);
+                }
+            }
+        } else {
+            response = text_response(http::status::method_not_allowed, "Invalid method"sv, ContentType::TEXT_HTML);
+            response.set("Allow"sv, "GET, HEAD"sv);
+            response.set(http::field::content_type, "text/html"sv);
+        }
+        send(response);
+    }
+
+    static constexpr std::string_view HEAD_STR{"HEAD"};
+    static constexpr std::string_view GET_STR{"GET"};
+private:
+    struct ContentType {
+        ContentType() = delete;
+        constexpr static std::string_view TEXT_HTML =   "text/html"sv;
+        constexpr static std::string_view APPLICATION_JSON =   "application/json"sv;
+    };
+    StringResponse MakeStringResponse(http::status status,
+                                        std::string_view body,
+                                        unsigned http_version,
+                                        bool keep_alive,
+                                        std::string_view content_type = ContentType::TEXT_HTML
+                                        ){
+        StringResponse response(status, http_version);
+        response.set(http::field::content_type, content_type);
+        response.body() = body;
+        response.content_length(body.size());
+        response.keep_alive(keep_alive);
+        return response;
+    }
+
+    std::string MapsListResponse(){
+        auto maps = game_.GetMaps();
+        std::string body{"["};
+        bool first = true;
+        for(model::Map map : maps){
+            if(first){
+                first = false;
+            } else {
+                body += ',';
+            }
+            body += '{';
+            body += std::format("\"{}\": \"{}\"", *map.GetId(), map.GetName());
+            body += "}";
+        }
+        body += "]";
+        return body;
+    }
+
+    std::string MapResponse(const std::string& map_id){
+        std::string body;
+        model::Map::Id id{map_id};
+        const model::Map* map_ptr = game_.FindMap(id);
+        if(map_ptr) {
+            body = map_ptr->ToJson();
+        } else {
+            body += "{\n";
+            body += "\t\"code\": \"mapNotFound\",\n";
+            body += "\t\"message\": \"Map not found\"";
+            body += "\n}";
+        }
+        return body;
+    }
+
+    std::string BadRequest(){
+        std::string body{"{\n"};
+        body += "\t\"code\": \"badRequest\",\n";
+        body += "\t\"message\": \"Bad request\"";
+        body += "\n}";
+        return body;
+    }
+    model::Game& game_;
+};
+
+
+}  // namespace http_handler
