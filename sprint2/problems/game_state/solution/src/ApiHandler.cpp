89c86972c0e9c6f19e6d5dd3094b39de10f7ebb8
suhostav <vsuhostav@gmail.com>
2024-08-14 10:16:00 +0300

    Game state 3

--- sprint2/problems/game_state/solution/src/ApiHandler.cpp
+++ sprint2/problems/game_state/solution/src/ApiHandler.cpp
@@ -184,7 +184,7 @@ namespace http_handler {
                     } else {
                         auto session = GetSessionByToken(req["Authorization"]);
                         if(session == nullptr){
-                            response = handlers_statics::UnknownTokenResponse(req.version(), req.keep_alive());
+                            response = handlers_statics::InvalidTokenResponse(req.version(), req.keep_alive());
                         } else {
                             if(IsGetRequest(req)) {
                                 body = PlayersResponse(session);
