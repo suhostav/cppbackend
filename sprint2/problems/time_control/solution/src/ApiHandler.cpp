a59c12cafc09b03114de3f385a89884e164b4367
suhostav <vsuhostav@gmail.com>
2024-08-19 13:27:45 +0300

    Time control 9

--- sprint2/problems/time_control/solution/src/ApiHandler.cpp
+++ sprint2/problems/time_control/solution/src/ApiHandler.cpp
@@ -279,7 +279,13 @@ std::string ApiHandler::StateResponse(const model::GameSession* session) const{
 std::string ApiHandler::ActionResponse(const StringRequest& req) const{
     auto session = const_cast<model::GameSession*>(GetSession(req));
     auto token = TryExtractToken(req);
-    boost::json::object req_body{boost::json::parse(req.body()).as_object()};
+    boost::json::object req_body;
+    req_body = boost::json::parse(req.body()).as_object();
+    // try{
+    //     req_body = boost::json::parse(req.body()).as_object();
+    // }catch(...){
+    //     throw BadRequestException("invalidArgument", "Failed to parse action request JSON");
+    // }
     char dir = (char)toupper(req_body["move"].as_string()[0]);
     if(!(dir == 'U' || dir == 'D' || dir == 'L' || dir == 'R')){
         throw BadRequestException("InvalidDirection","Invalid Direction");
