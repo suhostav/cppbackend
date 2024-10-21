c1ad25809a0f5ecb71251330f9bd272c631411f2
suhostav <vsuhostav@gmail.com>
2024-10-21 16:14:53 +0300

    find return 1

---
+++ sprint3/problems/find_return/solution/src/GameApp.cpp
@@ -0,0 +1,59 @@
+#include "GameApp.h"
+
+#include <iostream>
+
+namespace app {
+
+using namespace collision_detector;
+
+const model::GameSession* GameApp::GetPlayerSession(const Token token) const{
+    Player* player = player_tokens_.FindPlayerByToken(token);
+    if(player_session_.count(player) == 0){
+        return nullptr;
+    }
+    return player_session_.at(player);
+}
+
+JoinInfo GameApp::JoinGame(std::string_view dog_name, std::string_view map_id_str){
+    model::JoinResult result = game_.JoinGame(dog_name, map_id_str);
+    Player* new_player = players_.Add(result.dog, result.session);
+    Token token = player_tokens_.Add(*new_player);
+    sessions_.insert(result.session);
+    player_session_[new_player] = result.session;
+    return {new_player, result.session, token};
+}
+
+void GameApp::SetPlayerSpeed(Token token, model::GameSession* session, char dir){
+    app::Player* player = FindPlayerByToken(token);
+    auto s = session->GetMap()->GeSpeed();
+    model::DogDir dog_dir = static_cast<model::DogDir>(dir);
+    DCoord limit = session->GetMap()->GetLimit(player->GetDog()->GetPoint2D(), dog_dir);
+    player->GetDog()->SetDir(dog_dir, limit);
+    switch(dog_dir){
+        case model::DogDir::WEST:
+            player->GetDog()->SetSpeed(-s, 0);
+            break;
+        case model::DogDir::EAST:
+            player->GetDog()->SetSpeed(s, 0);
+            break;
+        case model::DogDir::NORTH:
+            player->GetDog()->SetSpeed(0, -s);
+            break;
+        case model::DogDir::SOUTH:
+            player->GetDog()->SetSpeed(0, s);
+            break;
+    }
+}
+
+    void GameApp::Move(std::chrono::milliseconds period){
+
+        players_.Move(period);
+        for(auto* session : sessions_){
+            session->CheckCollisions();
+            session->ClearLoots();
+            session->GenerateLoots(period);
+        }
+    }
+
+
+}   //namespace app
