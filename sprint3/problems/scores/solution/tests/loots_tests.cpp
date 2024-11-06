ebc779f710393f8787b7e64721178a921b2bde23
suhostav <vsuhostav@gmail.com>
2024-11-06 16:27:17 +0300

    score3

---
+++ sprint3/problems/scores/solution/tests/loots_tests.cpp
@@ -0,0 +1,16 @@
+#define _USE_MATH_DEFINES
+
+#include <vector>
+#include <sstream>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_contains.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include "../src/collision_detector.h"
+
+using namespace collision_detector;
+using namespace std::literals;
+using Catch::Matchers::WithinAbs;
+
+SCENARIO("Take loot test."){
+    
+}
